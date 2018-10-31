f7c97f078ed5e6044a6ec9ccec7ccc6f66a0653e
liverbool <nukboon@gmail.com>
2018-10-31 17:43:03 +0700

    env.prod

--- public/index.php
+++ public/index.php
@@ -4,18 +4,12 @@ use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__.'/../vendor/autoload.php';
-
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-    }
-    (new Dotenv())->load(__DIR__.'/../.env');
-}
+require __DIR__ . '/../vendor/autoload.php';
 
 $env = $_SERVER['APP_ENV'] ?? 'dev';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
+$debug = (bool)($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
+
+(new Dotenv())->load(__DIR__ . '/../.env' . ('dev' === $env ? '' : '.' . $env));
 
 if ($debug) {
     umask(0000);
